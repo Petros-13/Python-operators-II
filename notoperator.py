5887c03e1a2bc96da07c70239f05fdff5f40485e
Petros-13 <petroskalkounos78@gmail.com>
2025-07-24 15:53:28 +0200

    2nd commit

---
+++ notoperator.py
@@ -0,0 +1,20 @@
+x=5
+y=7
+if x!=7:
+    print("x is not 7")
+elif x==7:
+     print("x is equal to 7")
+else:
+     print("x is not equals to 7")
+
+if y!=7:
+     print("y is not 7")
+elif y==7:
+     print("y is equal to 7")
+else:
+     print("Is not 7")
+name=(int(input("Enter a number")))
+if name%2!=0:
+     print("The number is odd")
+else:
+     print("The number is even")
